088a25605a45b1342945585f5cb204fb22ac7a47
dashameter <dashameter@gmail.com>
2022-05-21 20:40:35 +0200

    fix more stuff

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+    resolve: {
+      alias: {
+        fs: false,
+      },
+    },
+  },
+};
